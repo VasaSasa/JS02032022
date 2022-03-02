0b7f36b94bf352e6c2deae0a6560d01c6cdc8143
Václav Doležal <bc.va.do@gmail.com>
2022-03-02 17:07:45 +0100

    propojení mezi soubory, základní struktura html, příklady v js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+alert('Výtejte na mých stránkách')
+
+let user = prompt('Jaké je vaše oblíbené číslo?: ')
+console.log(user)
+
+
+cislo10 = 10
+
+console.log(cislo10 + 3)
+console.log(cislo10 * 5)
+console.log(cislo10 - 30)
+console.log(cislo10 / 7)
+
+
